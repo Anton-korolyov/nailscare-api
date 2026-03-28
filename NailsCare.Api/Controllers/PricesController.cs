434933791db1e73c309359386c1a663568e01dfd
anton <anton@LAPTOP-B1GN23L2>
2026-03-28 10:30:18 +0300

    add prices

---
+++ NailsCare.Api/Controllers/PricesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NailsCare.Api.Data;
+using NailsCare.Api.Models;
+
+namespace NailsCare.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PricesController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+
+        public PricesController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        // ================= GET ALL PRICES =================
+        [HttpGet]
+        public async Task<ActionResult<List<Price>>> GetPrices()
+        {
+            var prices = await _db.Prices.ToListAsync();
+            return Ok(prices);
+        }
+    }
+}
